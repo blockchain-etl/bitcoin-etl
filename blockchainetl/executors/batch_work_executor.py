33d6ac83ca93764a13b31a1a880ef37a7d7cbe97
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-07 00:51:30 +0700

    Improve batch_work_executor.py

--- blockchainetl/executors/batch_work_executor.py
+++ blockchainetl/executors/batch_work_executor.py
@@ -19,14 +19,17 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import time
+from http.client import HTTPException
 
 from requests.exceptions import Timeout as RequestsTimeout, HTTPError, TooManyRedirects
+
 from blockchainetl.executors.bounded_executor import BoundedExecutor
 from blockchainetl.executors.fail_safe_executor import FailSafeExecutor
 from blockchainetl.progress_logger import ProgressLogger
 from blockchainetl.utils import dynamic_batch_iterator
 
-RETRY_EXCEPTIONS = (ConnectionError, HTTPError, RequestsTimeout, TooManyRedirects, OSError)
+RETRY_EXCEPTIONS = (ConnectionError, HTTPError, RequestsTimeout, TooManyRedirects, OSError, HTTPException)
 
 
 # Executes the given work in batches, reducing the batch size exponentially in case of errors.
@@ -47,16 +50,19 @@ class BatchWorkExecutor:
 
     # Check race conditions
     def _fail_safe_execute(self, work_handler, batch):
-        try:
-            work_handler(batch)
-        except self.retry_exceptions:
-            batch_size = self.batch_size
-            # Reduce the batch size. Subsequent batches will be 2 times smaller
-            if batch_size == len(batch) and batch_size > 1:
-                self.batch_size = int(batch_size / 2)
-            # For the failed batch try handling items one by one
-            for item in batch:
-                work_handler([item])
+        retries = 3
+        delay = 3
+        for i in range(retries):
+            try:
+                time.sleep(delay)
+                work_handler(batch)
+            except self.retry_exceptions as exception:
+                delay = delay * 2
+                print('Exception occurred {}. Waiting {} seconds'.format(type(exception), delay))
+                if i < retries - 1:
+                    continue
+                else:
+                    raise
         self.progress_logger.track(len(batch))
 
     def shutdown(self):
