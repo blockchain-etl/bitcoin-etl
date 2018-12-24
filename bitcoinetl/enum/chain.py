f38cf206136b79a2f3e2e731dba3e589d18c9d2d
Evgeny Medvedev <medvedev1088@gmail.com>
2018-12-24 22:16:41 +0700

    Update comment

--- bitcoinetl/enum/chain.py
+++ bitcoinetl/enum/chain.py
@@ -5,4 +5,5 @@ class Chain:
     LITECOIN = 'litecoin'
 
     ALL = [BITCOIN, BITCOIN_CASH, DOGECOIN, LITECOIN]
+    # Old API doesn't support verbosity for getblock which doesn't allow querying all transactions in a block in 1 go.
     HAVE_OLD_API = [BITCOIN_CASH, DOGECOIN]
