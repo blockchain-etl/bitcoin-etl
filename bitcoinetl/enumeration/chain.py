794a136bef6c02a8493883b3807c30226725b6d7
tokusyu <tokusyu@gmail.com>
2019-06-23 00:38:13 +0900

    add bitoin_gold

--- bitcoinetl/enumeration/chain.py
+++ bitcoinetl/enumeration/chain.py
@@ -6,7 +6,8 @@ class Chain:
     DASH = 'dash'
     ZCASH = 'zcash'
     MONACOIN = 'monacoin'
+    BITCOIN_GOLD = 'bitcoin_gold'
 
-    ALL = [BITCOIN, BITCOIN_CASH, DOGECOIN, LITECOIN, DASH, ZCASH, MONACOIN]
+    ALL = [BITCOIN, BITCOIN_CASH, DOGECOIN, LITECOIN, DASH, ZCASH, MONACOIN, BITCOIN_GOLD]
     # Old API doesn't support verbosity for getblock which doesn't allow querying all transactions in a block in 1 go.
     HAVE_OLD_API = [BITCOIN_CASH, DOGECOIN, DASH, MONACOIN]
