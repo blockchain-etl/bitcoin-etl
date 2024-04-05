cb07c9769c55427be0b060d6cc7bebea4908d5e2
virtu <virtu@cryptic.to>
2024-04-05 14:22:26 +0200

    Rename bitcoinetl to bitcoin-etl

--- module.nix
+++ module.nix
@@ -3,10 +3,10 @@
 with lib;
 
 let
-  cfg = config.services.bitcoinetl;
+  cfg = config.services.bitcoin-etl;
 in
 {
-  options.services.bitcoinetl = {
+  options.services.bitcoin-etl = {
     enable = mkEnableOption "Bitcoin ETL service";
 
     package = mkOption {
@@ -29,23 +29,23 @@ in
       groups.bitcoin-etl = { };
     };
 
-    systemd.timers.bitcoinetl = {
+    systemd.timers.bitcoin-etl = {
       description = "Timer for Bitcoin ETL Service";
       wantedBy = [ "timers.target" ];
-      after = [ "bitcoinetl.service" ];
+      after = [ "bitcoin-etl.service" ];
       timerConfig = {
         OnCalendar = "hourly";
-        Unit = "bitcoinetl.service";
+        Unit = "bitcoin-etl.service";
       };
     };
 
-    systemd.services.bitcoinetl = {
+    systemd.services.bitcoin-etl = {
       description = "Bitcoin ETL Service";
       # service should only run when scheduled, not when system is booted so no
       # wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
       script = ''
-        ${cfg.package}/bin/bitcoinetl \
+        ${cfg.package}/bin/bitcoin-etl \
           ${if cfg.exportBlocks then "--export-blocks" else "--no-export-blocks"} \
       '';
 
@@ -57,6 +57,6 @@ in
         User = "bitcoin-etl";
         Group = "bitcoin-etl";
       };
-    }; # systemd.services.bitcoinetl
+    }; # systemd.services.bitcoin-etl
   }; # config
 }
